ed7075315ad0a047f5cd4be973a77c1d97e12e7e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-07 10:46:15 +0200

    refactor: catch error if the user enters non letters chars as input

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ data = pandas.read_csv("./nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ").upper()
-phonetic_word = [nato_dict[letter] for letter in user_input]
-print(phonetic_word)
+def generate_phonetic():
+  user_input = input("Enter a word: ").upper()
+
+  try:
+    phonetic_word = [nato_dict[letter] for letter in user_input]
+  except KeyError:
+    print("Sorry, only letters in the alphabet please.")
+    generate_phonetic()
+  else:
+    print(phonetic_word)
+
+generate_phonetic()
